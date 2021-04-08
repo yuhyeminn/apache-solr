c3ea68d9ebf214c95a7444b3ea375d0eb2da95df
hyemin <yuhyemin96@gmail.com>
2021-04-08 17:24:26 +0900

    modify SolrClient

--- solr_bbs/src/main/java/com/jpa/bbs/common/SolrJDriver.java
+++ solr_bbs/src/main/java/com/jpa/bbs/common/SolrJDriver.java
@@ -3,7 +3,7 @@ package com.jpa.bbs.common;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
-public class SolrDriver {
-    public static String url = "https://localhost:8983/solr/board";
+public class SolrJDriver {
+    public static String url = "http://localhost:8983/solr/board";
     public static SolrClient solr = new HttpSolrClient.Builder(url).build();
 }
